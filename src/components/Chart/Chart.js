d0064b055d26aec8bc5c6c438fc220943e3802c6
yeonjin1357 <yeonjin1357@naver.com>
2023-11-29 19:07:19 +0900

    11/29 리액트 공부

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,11 +4,14 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
-      {props.dataPoints.map((dataPoint) => {
-        <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={null} label={dataPoint.label}></ChartBar>;
-      })}
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label}></ChartBar>
+      ))}
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -20,6 +21,7 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
+        <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
         <ExpensesList items={filteredExpenses}></ExpensesList>
       </Card>
     </div>
