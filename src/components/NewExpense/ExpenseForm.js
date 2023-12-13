aba5b1f1b14218e15d2485df1fda4a7c399160d5
yeonjin1357 <yeonjin1357@naver.com>
2023-12-13 19:19:50 +0900

    23/12/13 리액트 공부

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -48,6 +48,7 @@ const ExpenseForm = (props) => {
       amount: +enteredAmount,
       date: new Date(enteredDate),
     };
+    //
 
     props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
